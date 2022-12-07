2f96046970c378acb939d7fb3c66638b3581427d
Roy <roy@madsq.net>
2022-12-07 17:51:18 +0900

    feat: twin.macro 초기 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,21 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/wedding-invitation-letter/',
-  plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'ESNext',
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
