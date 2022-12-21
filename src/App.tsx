40694caa3bccd0bd96b4b4b99cea7de8a80b300b
Roy <roy@madsq.net>
2022-12-21 18:44:26 +0900

    221221

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import tw, { styled } from 'twin.macro';
+
 const App = () => {
-  return <div className="App">모바일 청첩장</div>;
+  return <StyledApp>모바일 청첩장!!</StyledApp>;
 };
 
 export default App;
+
+const StyledApp = styled.div`
+  ${tw`flex w-screen h-screen justify-center items-center`}
+`;
