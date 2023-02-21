76f1f12716a73d6375d46cc9332d3fbd99526193
Roy <roy@madsq.net>
2023-02-21 18:55:32 +0900

    230221

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import tw, { styled } from 'twin.macro';
+import MainBanner from './components/mainBanner';
 
 const App = () => {
-  return <StyledApp>모바일 청첩장!!</StyledApp>;
+  return (
+    <StyledApp>
+      <MainBanner />
+    </StyledApp>
+  );
 };
 
 export default App;
 
 const StyledApp = styled.div`
-  ${tw`flex w-screen h-screen justify-center items-center`}
+  ${tw`max-w-5xl mx-auto h-screen`}
+
+  @supports (-webkit-touch-callout: none) {
+    height: -webkit-fill-available;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
 `;
---
+++ src/components/mainBanner.tsx
@@ -0,0 +1,29 @@
+import { FC, HTMLProps } from 'react';
+import tw, { styled } from 'twin.macro';
+
+type MainBannerProps = HTMLProps<HTMLDivElement>;
+
+const MainBanner: FC<MainBannerProps> = () => {
+  return (
+    <StyledMainBanner className="relative">
+      <img className="h-full object-cover" src="images/main_banner.jpg" alt="main banner" />
+      <div className="absolute bottom-48 left-[50%] translate-x-[-50%]  text-center text-white">
+        <p className="mb-8 border border-solid border-white py-4">
+          소중한
+          <br />
+          당신을
+          <br />
+          모십니다
+        </p>
+        <p className="mb-2">임수현 • 김은영</p>
+        <p>2023.08.26</p>
+      </div>
+    </StyledMainBanner>
+  );
+};
+
+export default MainBanner;
+
+const StyledMainBanner = styled.section`
+  ${tw`bg-[#F7F7F7] h-full`}
+`;
