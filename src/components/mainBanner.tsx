a2ddc665de955f225bd4efabd803dbe9f5c611ec
Roy <roy@madsq.net>
2023-02-21 18:59:35 +0900

    230221-메인배너 작업

--- src/components/mainBanner.tsx
+++ src/components/mainBanner.tsx
@@ -6,7 +6,7 @@ type MainBannerProps = HTMLProps<HTMLDivElement>;
 const MainBanner: FC<MainBannerProps> = () => {
   return (
     <StyledMainBanner className="relative">
-      <img className="h-full object-cover" src="images/main_banner.jpg" alt="main banner" />
+      <img className="h-full w-full object-cover" src="images/main_banner.jpg" alt="main banner" />
       <div className="absolute bottom-48 left-[50%] translate-x-[-50%]  text-center text-white">
         <p className="mb-8 border border-solid border-white py-4">
           소중한
