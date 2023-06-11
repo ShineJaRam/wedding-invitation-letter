86f9b9ea1743564c64118c055eba936dc5cfd382
SuhyunLim <suhuyn19@naver.com>
2023-06-11 23:01:14 +0900

    230611-1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <article className="mx-auto max-w-5xl bg-[#F2F1F6] px-4">
       <MainBanner />
       <Greetings />
-      <img className="mx-[-16px] w-full max-w-none" src="images/img2.jpg" alt="우리가 강아지를 끌어안고 있는 사진" />
+      <img className="mx-[-16px] w-screen max-w-none" src="images/img2.jpg" alt="우리가 강아지를 끌어안고 있는 사진" />
     </article>
   );
 };
