270e58997e3e86bed04d7108c1a9d64d62ea4eeb
SuhyunLim <suhuyn19@naver.com>
2023-07-02 23:19:43 +0900

    230702-1

--- src/components/gallary.tsx
+++ src/components/gallary.tsx
@@ -1,4 +1,7 @@
 const Gallary = () => {
+  // cdn file path
+  // https://cdn.jsdelivr.net/gh/ShineJaRam/wedding-invitation-letter@gh-pages/images/gallary/picture-000.jpg
+
   return (
     <div className="py-5">
       <h1>갤러리</h1>
