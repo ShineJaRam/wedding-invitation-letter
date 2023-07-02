9cc100def0bb0c91a5724ce7837b7726fa1f7483
SuhyunLim <suhuyn19@naver.com>
2023-07-02 23:38:46 +0900

    230702-2

--- src/components/gallary.tsx
+++ src/components/gallary.tsx
@@ -1,10 +1,26 @@
 const Gallary = () => {
   // cdn file path
   // https://cdn.jsdelivr.net/gh/ShineJaRam/wedding-invitation-letter@gh-pages/images/gallary/picture-000.jpg
-
+  const files = [...Array(47)].map(() => 0);
   return (
     <div className="py-5">
       <h1>갤러리</h1>
+
+      <div className="flex flex-wrap gap-2">
+        {files.map((_, idx) => {
+          const _idx = idx < 10 ? `0${idx}` : idx;
+
+          return (
+            <div key={idx} className="h-[109px] w-[109px] overflow-hidden">
+              <img
+                className="object-cover"
+                src={`https://cdn.jsdelivr.net/gh/ShineJaRam/wedding-invitation-letter@gh-pages/images/gallary/picture-0${_idx}.jpg`}
+                alt="wedding gallary"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
