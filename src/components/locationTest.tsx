9e4aa438bfd4fbd4a6b7080a830c00d80f2015d1
SuhyunLim <suhuyn19@naver.com>
2023-07-12 23:04:45 +0900

    230712-6

--- src/components/locationTest.tsx
+++ src/components/locationTest.tsx
@@ -1,12 +1,15 @@
 import { isMobile } from 'react-device-detect';
 
 const LocationTest = () => {
+  const lat = 37.559116633916;
+  const lng = 126.984455216505;
+
   return (
     <div>
       <a
         href={`${
           isMobile
-            ? 'nmap://place?lat=37.559116633916&lng=126.984455216505&name=라루체 웨딩&appname=https://shinejaram.github.io/wedding-invitation-letter/'
+            ? `nmap://place?lat=${lat}&lng=${lng}&name=라루체 웨딩&appname=https://shinejaram.github.io/wedding-invitation-letter/`
             : 'https://naver.me/xcKJWnge'
         }`}
         target="_blank"
@@ -15,7 +18,11 @@ const LocationTest = () => {
         <button>네이버 지도</button>
       </a>
 
-      <a href={`${isMobile ? '' : 'http://kko.to/nR1-GIqr2D'}`} target="_blank" rel="noreferrer noopener">
+      <a
+        href={`${isMobile ? `kakaomap://route?ep=${lat},${lng}&by=CAR` : 'http://kko.to/nR1-GIqr2D'}`}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         <button>카카오 지도</button>
       </a>
     </div>
