f62c0f670e2cff884c726d66c7a47f9c77d37f0a
Roy <roy@madsq.net>
2023-07-26 10:16:16 +0900

    230726-1

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,8 +1,8 @@
-import { styled } from 'twin.macro';
 import modalStore from '@/store/modalStore';
 import { FC, PropsWithChildren, useRef } from 'react';
 import useOutSideClick from '@/hooks/useOutSideClick';
 import useBlockScroll from '@/hooks/useBlockScroll';
+import '@styles/modal.css';
 
 interface ModalProps extends PropsWithChildren {
   key?: string | number;
@@ -17,30 +17,12 @@ const Modal: FC<ModalProps> = ({ children }) => {
   useBlockScroll();
 
   return (
-    <Overlay>
-      <ModalWrap ref={ref}>{children}</ModalWrap>
-    </Overlay>
+    <div className="overlay">
+      <div className="modal-wrap" ref={ref}>
+        {children}
+      </div>
+    </div>
   );
 };
 
 export default Modal;
-
-const Overlay = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.2);
-  z-index: 9999;
-`;
-
-const ModalWrap = styled.div`
-  border-radius: 15px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
